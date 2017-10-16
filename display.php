4aa8fe3944e87a6ab3dd0c01c20c62b422aeeccd
Harshit Mayur Shah <hs574@afsconnect2.njit.edu>
2017-10-16 01:13:27 -0400

    recent commit

--- display.php
+++ display.php
@@ -3,7 +3,7 @@ echo "<html><body><table border=1>\n\n";
 $name= $_SERVER['DOCUMENT_ROOT']."/upload/".$_GET['name'];
 
 
-
+echo $name;
 $f = fopen($name,"r");
 while (($line = fgetcsv($f)) !== false) {
         echo "<tr>";
