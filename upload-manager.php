d836cbca2a3821e0c219c67d6f308a09bb587ed3
Harshit Mayur Shah <hs574@afsconnect1.njit.edu>
2017-10-16 15:23:07 -0400

    recent commit

--- upload-manager.php
+++ upload-manager.php
@@ -24,7 +24,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             } else{
                 echo $_SERVER['DOCUMENT_ROOT']. "/upload/" . $_FILES["photo"]["name"];
                 echo "\n"; 
-                move_uploaded_file($_FILES["photo"]["tmp_name"],"https://afs/cad.njit.edu/u/h/s/hs574/public_html/uploads/" . $_FILES["photo"]["name"]);
+                move_uploaded_file($_FILES["photo"]["tmp_name"],"https://web.njit.edu/~hs574/public_html/uploads/" . $_FILES["photo"]["name"]);
                 echo "Your file was uploaded successfully.";
                   
 
