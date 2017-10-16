46f921f093393497a9986397970d8767da5f2f53
Harshit Mayur Shah <hs574@afsconnect2.njit.edu>
2017-10-16 00:24:03 -0400

    recent commit

--- upload-manager.php
+++ upload-manager.php
@@ -26,7 +26,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 echo "Your file was uploaded successfully.";
 
 
-                   header("Location:Location:https://web.njit.edu/~hs574/project2/display.php?name=$filename");
+                   header("Location:https://web.njit.edu/~hs574/project2/display.php?name=$filename");
 
             }
 
